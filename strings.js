8c6fb161aec0dcb6f1a3b9bc5e792846397a5b14
bennu <benarzee1212@gmail.com>
2021-10-12 08:12:51 +0530

    new

---
+++ strings.js
@@ -0,0 +1,10 @@
+var myStr="JAVA SCRIPT"
+console.log(myStr)
+console.log(typeof(myStr))
+
+// to display double string
+var myStr="JAVA SCRIPT is a \'scripting\' language"
+console.log(myStr)
+var myStr2="JAVA SCRIPT is a \"scripting\" language"
+console.log(myStr2)
+
